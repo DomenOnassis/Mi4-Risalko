93875b4c0d2292762e5b75eae8ca438a3f920d74
AnejBradacVranc <mikedrifner@gmail.com>
2025-10-17 23:59:44 +0200

    feat: Started adding basic CRUD operations, added MongoDB driver, started adding endpoints for getting and creating documents in db, added populate functionallity, added basic structure for backend, added requirements.txt for running with venv

---
+++ backend/db.py
@@ -0,0 +1,82 @@
+from pymongo import MongoClient, ReturnDocument
+from dotenv import load_dotenv
+from datetime import datetime
+
+import os
+
+load_dotenv()
+
+DB_URI= os.getenv("DB_URI")
+
+class Connection:        
+    def __init__(self, database):
+        client = MongoClient(DB_URI)
+        self.db = client[database]
+	
+    def find_all(self, collection_name):
+        try:            
+            collection = self.db[collection_name]
+            documents = list(collection.find({}, {'_id': 0}))
+            return documents
+        except Exception as e:
+            print("Error fetching documents:", e)
+            return []
+    
+    def lookup_all(self, collection_name, pipeline):
+        if  not pipeline:
+            print("No filter provided when updating element")
+            return None
+        
+        try:        
+            
+            collection = self.db[collection_name]
+            documents = list(collection.aggregate(pipeline=pipeline))
+            return documents
+        except Exception as e:
+            print("Error fetching documents:", e)
+            return []
+        
+        
+    def insert(self, collection_name, item):
+        try:
+            item['created_at'] = datetime.now()
+            collection = self.db[collection_name]
+            result = collection.insert_one(item)             
+            return result.inserted_id
+        except Exception as e:
+            print("Error inserting document:", e)
+            return None
+    
+    def update_one(self, collection_name, item, filter_query, append_array = False):
+      if(not filter_query):
+          print("No filter provided when updating element")
+          return None
+
+      try:
+          collection = self.db[collection_name]
+          
+          update = {
+            "$currentDate": {"updated_at": True}
+            }
+        
+          if append_array:
+              update["$push"] = item
+          else:
+              update["$set"] = item
+                     
+          filter_query = filter_query 
+          result = collection.find_one_and_update(
+              filter_query,
+              update,
+              return_document=ReturnDocument.AFTER
+          )
+          if result:
+              return result
+          else:
+              print("No matching document found to update.")
+              return None					
+      except Exception as e:
+          print("Error updating one document:", e)
+          return None
+
+
--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,164 @@
-# TODO
+from flask import Flask, Response,request, Blueprint
+from bson import json_util, ObjectId
+from db import Connection
+import validator
+
+app = Flask(__name__)
+
+api = Blueprint('api', __name__, url_prefix='/api')
+
+db = Connection("risalko")
+
+#User
+@api.get("/users")
+def get_users():
+    populate = request.args.get("populate", False)
+    
+    if populate:
+        pipeline = [                
+            {"$lookup": {
+                        "from": "stories",
+                        "localField": "stories",
+                        "foreignField": "_id",
+                        "as": "stories"
+                    }
+            },     
+        ]
+        users = db.lookup_all("users",pipeline)
+    else:
+        users = db.find_all("users")
+
+    return Response(
+        json_util.dumps({"data": list(users)}),
+        mimetype='application/json'
+    ), 200
+    
+@api.post("/user")
+def create_user():
+    data = request.get_json()
+        
+    missing_fields = validator.validate_required_fields(data, ["name, surname"])
+    
+    if missing_fields:
+        return Response({
+            'error': f'Missing required fields: {", ".join(missing_fields)}'
+        }), 400
+    
+    user = {
+        'name': data.get("name"),
+        'surname': data.get("surname"),
+        'type': data.get("type", "student"),
+    }
+    
+    db.insert("users", user)    
+   
+    return Response(
+        json_util.dumps({"data": user}),
+        mimetype='application/json'
+    ), 200
+
+#Story   
+@api.get("/stories")
+def get_stories():
+    
+    stories = db.find_all("stories")
+    return Response(
+        json_util.dumps({"data": list(stories)}),
+        mimetype='application/json'
+    ), 200
+    
+@api.post("/story")
+def create_story():
+    data = request.get_json()
+        
+    missing_fields = validator.validate_required_fields(data, ["title, author, short_description, content, teacher_id"])
+    
+    if missing_fields:
+        return Response({
+            'error': f'Missing required fields: {", ".join(missing_fields)}'
+        }), 400
+        
+    teacher_id = data.get("teacher_id")
+    
+    story = {
+        'title': data.get("title"),
+        'author': data.get("author"),
+        'short_description': data.get("short_description"),
+        'content': data.get('content'),
+    }
+    
+    try:
+        teacher_object_id = ObjectId(teacher_id)
+    except Exception:
+        return Response({'error': 'Invalid teacher_id format'}), 400
+    
+    inserted_story = db.insert("stories", story)    
+    
+    update_user = db.update_one("user", {'stories': inserted_story['_id']}, {'_id': teacher_object_id}, append_array=True)
+    
+    if(not update_user):
+                return Response({'error': 'Could not append story to user'}), 400
+           
+    return Response(
+        json_util.dumps({"data": inserted_story}),
+        mimetype='application/json'
+    ), 200
+    
+#Class
+@api.get("/classes")
+def get_classes():
+    populate = request.args.get("populate", False)
+    if populate:
+        pipeline = [                
+            {"$lookup": {
+                        "from": "users",
+                        "localField": "students",
+                        "foreignField": "_id",
+                        "as": "students"
+                    }
+            },
+             {"$lookup": {
+                        "from": "users",
+                        "localField": "teacher",
+                        "foreignField": "_id",
+                        "as": "teacher"
+                    }
+            }
+        ]
+        classes = db.lookup_all("classes",pipeline)
+
+    else:
+        classes = db.find_all("classes")
+      
+    return Response(
+        json_util.dumps({"data": list(classes)}),
+        mimetype='application/json'
+    ), 200
+    
+@api.post("/class")
+def create_class():
+    data = request.get_json()
+        
+    missing_fields = validator.validate_required_fields(data, ["students, teacher"])
+    
+    if missing_fields:
+        return Response({
+            'error': f'Missing required fields: {", ".join(missing_fields)}'
+        }), 400
+    
+    user = {
+        'students': data.get("students"),
+        'teacher': data.get("teacher"),   
+    }
+    
+    db.insert("classes", user)    
+   
+    return Response(
+        json_util.dumps({"data": user}),
+        mimetype='application/json'
+    ), 200
+    
+
+if __name__=="__main__":
+    app.register_blueprint(api)
+    app.run(debug=True)
